cb72a6bf46aa5d112d41337edd77da8fc24037b0
nini <zjc0507@gmail.com>
2019-05-28 17:59:47 +0800

    current location with train stations -- Putting it all together

--- app/src/main/java/com/example/googlemaps2/MapsActivity.java
+++ app/src/main/java/com/example/googlemaps2/MapsActivity.java
@@ -22,12 +22,47 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
+import android.graphics.Color;
+import android.graphics.Point;
+import android.graphics.PointF;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private FusedLocationProviderClient mFusedLocationProviderClient;
     Location currentLocation;
     private static int REQUEST_CODE = 101;
+
+
+    private String TrainStationName;
+    private String STOPLAT;
+    private String STOPLON;
+    private ArrayList<PointD> latlngs;
 
 
     @Override
@@ -43,6 +78,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         getDeviceLocation();
 
 
+        latlngs = new ArrayList<PointD>();
+        getTrainstationLocation();
     }
 
     private void getDeviceLocation() {
@@ -93,9 +130,77 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         System.out.println("onMap: "+latLng);
         MarkerOptions markerOptions=new MarkerOptions().position(latLng).title("I am here!");
         googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
-        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng ,5));
         googleMap.addMarker(markerOptions);
+
+        for(int i = 0; i < latlngs.size(); i++) {
+            setLocation(googleMap, latlngs.get(i));
+        }
+
     }
+
+    private void setLocation(GoogleMap googleMap, PointD point) {
+        final LatLng trainstation = new LatLng(point.lat, point.lng);
+
+        final Marker marker = googleMap.addMarker(new MarkerOptions()
+                .position(trainstation)
+                .title(point.name));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(trainstation));
+    }
+
+
+    public void  getTrainstationLocation(){
+        final RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        String url ="http://data-atgis.opendata.arcgis.com/datasets/c82756c875ff4e9fad0bc7a9f97ef7a8_0.geojson";
+
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest(Request.Method.GET, url, null, new com.android.volley.Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                StringBuilder localities = new StringBuilder();
+
+                try {
+                    JSONArray data = response.getJSONArray("features");
+
+                    for (int index = 0; index < data.length(); index++) {
+                        JSONObject station = data.getJSONObject(index);
+
+                        //JSONObject geometry = station.getJSONObject("geometry");
+                        JSONObject properties = station.getJSONObject("properties");
+
+                        TrainStationName = properties.getString("STOPNAME");
+                        STOPLAT = properties.getString("STOPLAT");
+                        STOPLON = properties.getString("STOPLON");
+
+                        localities.append(properties.getString("STOPNAME") + "\n");
+                        localities.append(properties.getString("STOPLAT") + "\n");
+                        localities.append(properties.getString("STOPLON") + "\n");
+                        double lat = Double.valueOf(STOPLAT);
+                        double lng = Double.valueOf(STOPLON);
+                        latlngs.add(new PointD(TrainStationName, lat, lng));
+                    }
+
+                    System.err.println(response);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                //output.setText(localities.toString());
+                //System.out.println(TrainStationName + " , " + STOPLAT +","+ STOPLON);
+                Log.i("info",localities.toString());
+
+                // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+                SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                        .findFragmentById(R.id.map);
+                mapFragment.getMapAsync(MapsActivity.this);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("error",error.getLocalizedMessage());
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(jsObjRequest);
+    }
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
---
+++ app/src/main/java/com/example/googlemaps2/PointD.java
@@ -0,0 +1,14 @@
+package com.example.googlemaps2;
+
+
+public class PointD {
+    public String name;
+    public double lat;
+    public double lng;
+
+    PointD(String name, double lat, double lng) {
+        this.name = name;
+        this.lat = lat;
+        this.lng = lng;
+    }
+}
